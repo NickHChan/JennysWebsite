01f9aefd7301a398b11dce88a126270a9a5f1ec0
Nick Chan <nickharuchan@gmail.com>
2024-05-12 18:22:46 -0400

    added severeal svgs for basic logos like phone and email, finished fifthbody component that details gps and basic resturant infomation

--- jennyswebsites/src/app/components/FirstTemplate/FifthBody/Fifthbody.tsx
+++ jennyswebsites/src/app/components/FirstTemplate/FifthBody/Fifthbody.tsx
@@ -1,8 +1,54 @@
 import React from 'react'
 import styles from './Fifthbody.module.css'
+import Image from 'next/image'
+import gpsImg from '../../../../../images/gps.png'
+import locationImg from '../../../../../images/location.svg'
+import phoneImg from '../../../../../images/phone.svg'
+import emailImg from '../../../../../images/email.svg'
+import clockImg from '../../../../../images/clock.svg'
 
 export default function Fifthbody() {
   return (
-    <div>Fifthbody</div>
+    <div className={styles.fifthbodyContainer}>
+      <Image
+      src={gpsImg}
+      alt='picture of resturant location on google maps'
+      height={500}
+      />
+      <section>
+        <h5 className={styles.title}>Jenny Thai Restaurant</h5>
+        <article className={styles.info}>
+          <Image
+          src={locationImg}
+          alt='Location SVG'
+          />
+          <p>Some Address</p>
+        </article>
+        <article className={styles.info}>
+        <Image
+          src={phoneImg}
+          alt='Location SVG'
+          />
+          <p>Some Phone#</p>
+        </article>
+        <article className={styles.info}>
+        <Image
+          src={emailImg}
+          alt='Location SVG'
+          />
+          <p>Some Email</p>
+        </article>
+        <article className={styles.info}>
+        <Image
+          src={clockImg}
+          alt='Location SVG'
+          />
+          <p>Hours of Operation:
+            <br/>
+            Mon - Sun 10:00 Am - 9:00 Pm
+          </p>
+        </article>
+      </section>
+    </div>
   )
 }
