d397f1f44d898db9d231f8f058260b5f93e55638
Nick Chan <nickharuchan@gmail.com>
2024-05-13 17:00:23 -0400

    added Logo to NavbarTwo component and styled the text under logo

--- jennyswebsites/src/app/components/SecondTemplate/NavbarTwo.tsx
+++ jennyswebsites/src/app/components/SecondTemplate/NavbarTwo.tsx
@@ -3,14 +3,21 @@ import styles from './NavbarTwo.module.css'
 import Image from 'next/image'
 import menuSvg from '../../../../images/menubar.svg'
 import closeSvg from '../../../../images/close.svg'
+import logoSvg from '../../../../images/logo.png'
+import { log } from 'console'
 
 export default function NavbarTwo() {
   return (
     <div className={styles.navbarTwoContainer}>
       <div className={styles.infoContainer}>
-        <div>
-          <p>1</p>
-          <p>Authentic Thai Kitchen</p>
+        <div className={styles.logoContainer}>
+          <Image
+          src={logoSvg}
+          alt='Resturants Logo'
+          height={120}
+          width={120}
+          />
+          <p className={styles.logoText}>Authentic Thai Kitchen</p>
         </div>
 
         <button className={styles.menuSvg}></button>
