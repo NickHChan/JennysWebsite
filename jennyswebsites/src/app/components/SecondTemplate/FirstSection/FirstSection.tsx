0c2fc9d46342cc66d6faca17277385b6a16e28a4
Nick Chan <nickharuchan@gmail.com>
2024-05-13 18:57:54 -0400

    change navbarTwo background color, added background img and text to FirstSection component + added darker version of pictures for better text contast

---
+++ jennyswebsites/src/app/components/SecondTemplate/FirstSection/FirstSection.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './FirstSection.module.css'
+
+export default function FirstSection() {
+  return (
+    <div className={styles.firstSectionContainer}>
+        <div className={styles.textContainer}>
+            <h2> Jenny&apos;s Thai Food 
+                <br/>
+                Authentic Thai Kitchen
+            </h2>
+        </div>
+    </div>
+  )
+}
--- jennyswebsites/src/app/templatetwo/page.tsx
+++ jennyswebsites/src/app/templatetwo/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavbarTwo from '../components/SecondTemplate/NavbarTwo'
+import FirstSection from '../components/SecondTemplate/FirstSection/FirstSection'
 
 export default function TemplateTwo() {
   return (
     <div>
-    <NavbarTwo/>  
+    <NavbarTwo/>
+    <FirstSection/>  
       page
     </div>
   )
