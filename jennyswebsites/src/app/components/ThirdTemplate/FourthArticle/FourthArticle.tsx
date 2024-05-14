8d35e582f0f4c9050daffdba6efc3dbc5eb33dc3
Nick Chan <nickharuchan@gmail.com>
2024-05-14 13:22:33 -0400

    added FourtArticle Component base, and added border color and size to center div

--- jennyswebsites/src/app/components/ThirdTemplate/FourthArticle/FourthArticle.tsx
+++ jennyswebsites/src/app/components/ThirdTemplate/FourthArticle/FourthArticle.tsx
@@ -3,6 +3,10 @@ import styles from './FourthArticle.module.css'
 
 export default function FourthArticle() {
   return (
-    <div className={styles.fourthArticleContainer}>FourthArticle</div>
+    <div className={styles.fourthArticleContainer}>
+        <div className={styles.leftPillar}></div>
+        <div className={styles.middlePillar}></div>
+        <div className={styles.rightPillar}></div>
+    </div>
   )
 }
