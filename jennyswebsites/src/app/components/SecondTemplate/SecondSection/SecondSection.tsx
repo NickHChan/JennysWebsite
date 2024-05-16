3a880277291490246b3e6b62985c5b5e4ef6d122
Nick Chan <nickharuchan@gmail.com>
2024-05-15 21:31:00 -0400

    Completed SecondSection component

---
+++ jennyswebsites/src/app/components/SecondTemplate/SecondSection/SecondSection.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './SecondSection.module.css'
+import Image from 'next/image'
+import foodImg from '../../../../../images/Gallery-6.png'
+
+export default function SecondSection() {
+  return (
+    <div className={styles.secondSectionContainer}>
+        <Image
+        src={foodImg}
+        alt='Picture of the menu'
+        height={1000}
+        className={styles.foodImage}
+        />
+    </div>
+  )
+}
--- jennyswebsites/src/app/templatetwo/page.tsx
+++ jennyswebsites/src/app/templatetwo/page.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import styles from './page.module.css'
 import NavbarTwo from '../components/SecondTemplate/NavbarTwo'
 import FirstSection from '../components/SecondTemplate/FirstSection/FirstSection'
+import SecondSection from '../components/SecondTemplate/SecondSection/SecondSection'
 
 export default function TemplateTwo() {
   return (
     <div className={styles.templateTwoContainer}>
     <NavbarTwo/>
-    <FirstSection/>  
+    <FirstSection/>
+    <SecondSection/>  
       page
     </div>
   )
