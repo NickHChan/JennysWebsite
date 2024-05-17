49ea82612f146261156901a0e913b8e3755e094a
Nick Chan <nickharuchan@gmail.com>
2024-05-17 02:23:28 -0400

    added state and timeout counter to select reviews and display them in thirdSection component

--- jennyswebsites/src/app/components/SecondTemplate/ThirdSection/ThirdSection.tsx
+++ jennyswebsites/src/app/components/SecondTemplate/ThirdSection/ThirdSection.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, {useState, useEffect} from 'react'
 import styles from './ThirdSection.module.css'
 import Image from 'next/image'
 import thaiStickImg from '../../../../../images/thai-sticks.jpg'
@@ -7,9 +8,30 @@ import riceNoodleImg from '../../../../../images//rice-noodles.jpg'
 import { reviews } from './reviews'
 
 export default function ThirdSection() {
+
+  const [currentIndex, setCurrentIndex] = useState<number>(0)
+
+  useEffect(()=>{
+    const timer = setTimeout(()=>{
+      if( currentIndex <= 5){
+        setCurrentIndex(currentIndex + 1)
+      } else {
+        setCurrentIndex(0);
+      }
+    },5000)
+
+    return(()=>{
+      clearTimeout(timer)
+    })
+  })
+
   return (
     <div className={styles.thirdSectionContainer}>
         <div className={styles.reviewSection1}>
+          <div className={styles.reviewContainer}>
+            <h4>{reviews[currentIndex].name}</h4>
+            <p>{reviews[currentIndex].review}</p>
+          </div>
             <Image
             src={redCurryImg}
             alt='Picture of red curry'
