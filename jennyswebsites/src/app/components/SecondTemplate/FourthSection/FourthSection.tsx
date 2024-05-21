c83000cd4d51f656ef72d020ef75d415091366e1
Nick Chan <nickharuchan@gmail.com>
2024-05-20 20:56:35 -0400

    finished styling and adding transition effects on fourthSection component

--- jennyswebsites/src/app/components/SecondTemplate/FourthSection/FourthSection.tsx
+++ jennyswebsites/src/app/components/SecondTemplate/FourthSection/FourthSection.tsx
@@ -5,6 +5,7 @@ export default function FourthSection() {
   return (
     <div className={styles.fourthSectionContainer}>
       <h5 className={styles.title}>Locations</h5>
+      <p className={styles.underscore}>___________________</p>
       <div className={styles.locationContainer}>
         <div className={styles.location1}>
           <p className={styles.locationName}>North Carolina</p>
@@ -16,7 +17,7 @@ export default function FourthSection() {
           <button className={styles.button}>Order Online</button>
           <button  className={styles.button}>View Menu</button>
         </div>
-        <div className={styles.location1}>
+        <div className={styles.location3}>
           <p className={styles.locationName}>Toronto</p>
           <button className={styles.button}>Order Online</button>
           <button  className={styles.button}>View Menu</button>
