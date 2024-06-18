1d6eb0036469c99ba79640f0834aba3c1b19e9a0
Nick Chan <nickharuchan@gmail.com>
2024-06-17 22:41:57 -0400

    added Jenny's Logo for her resturant, created onMouseEnter and onMouseLeave state management to change logo to thai and english depending on state

--- jennyswebsites/src/app/components/FirstTemplate/Navbar/Navbar.tsx
+++ jennyswebsites/src/app/components/FirstTemplate/Navbar/Navbar.tsx
@@ -1,17 +1,23 @@
-import React from 'react'
+'use client'
+import React, {use, useState} from 'react'
 import styles from "./Navbar.module.css"
 import Image from 'next/image'
-import logoSvg from '../../../../../images/logo.png'
+import logoSvg from '../../../../../images/SEN-MEE-Logo-Light-Mode.png'
+import thaiLogoSvg from '../../../../../images/SEN-MEE-Thai-Logo-Light-Mode.png'
 
 function Navbar() {
+  const [isHovered, setIsHovered] = useState<boolean>(false)
+
   return (
     <div className={styles.navContainer}>
       <div className={styles.Container}>
           <Image
-          src={logoSvg}
+          src={ isHovered ? logoSvg : thaiLogoSvg}
           alt='Logo for resturant'
           height={120}
           width={120}
+          onMouseEnter={ () =>{ setIsHovered(true); console.log(isHovered)}}
+          onMouseLeave={() =>{ setIsHovered(false); console.log(isHovered)}}
           />
           <div className={styles.optionsContainer}>
               <div>Home</div>
