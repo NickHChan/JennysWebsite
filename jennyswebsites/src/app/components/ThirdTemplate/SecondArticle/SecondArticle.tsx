ef03c23f0d8acd7b55a8b7615378b102f058ebc4
Nick Chan <nickharuchan@gmail.com>
2024-06-18 02:08:03 -0400

    Added Mission statement for SecondArticle component, and changed img to resturant logo

--- jennyswebsites/src/app/components/ThirdTemplate/SecondArticle/SecondArticle.tsx
+++ jennyswebsites/src/app/components/ThirdTemplate/SecondArticle/SecondArticle.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './SecondArticle.module.css'
 import Image from 'next/image'
-import foodImg from '../../../../../images/woman-bringing-dish-to-table-2023-11-27-04-51-42-utc.jpg'
+import foodImg from '../../../../../images/SEN-MEE-Thai-Logo-Dark-Mode.png'
 
 export default function SecondArticle() {
   return (
@@ -9,20 +9,18 @@ export default function SecondArticle() {
       <div className={styles.leftPillar}></div>
       <div className={styles.secondArticleTextContainer}>
         <h3 className={styles.secondArticleText}>
-          Inspired by traditional flavors,
-          <br/>
-          Jenny&apos;s Thai Food offers a fresh,
-          <br/>
-          creative and flavorful
-          <br/>
-          interpretation of Thai cruisine.
+        Our mission is to provide delicious quality food at an affordable price, in a fast casual dining experience.
+        <br/>
+        Wowing your taste buds with salty, sweet, and spicy sour all in one bite. 
+        <br/>
+        Utilizing our authentic flavors from Southeast Asia, we aim to provide seasonal ingredients, imported herbs, and spices throughout our rotating menu.
         </h3>
         <button className={styles.button}>See Menu</button>
         <Image
         src={foodImg}
         alt='woman bringing thai food to the table'
         height={300}
-        width={450}
+        width={300}
         />
       </div>
       <div className={styles.rightPillar}></div>
